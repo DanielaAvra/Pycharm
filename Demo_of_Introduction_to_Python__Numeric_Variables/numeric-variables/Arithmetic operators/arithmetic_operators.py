62be080989e19a9686b00652ed67b023251fa8fb
Your Name <you@example.com>
2024-04-11 18:20:21 +0300

    Python

---
+++ Demo_of_Introduction_to_Python__Numeric_Variables/numeric-variables/Arithmetic operators/arithmetic_operators.py
@@ -0,0 +1,9 @@
+number = 9.0        # A number.
+
+division_result = 9.0 / 2
+
+multiplication_result = 9.0 * 2
+
+addition_result = division_result + multiplication_result
+
+print("result = " + str(addition_result))
---
+++ Introduction to Python/Variables/Arithmetic operators/arithmetic_operators.py
@@ -0,0 +1,17 @@
+init_number = 9.0        # Float number
+
+division_result = 9.0 / 2
+
+division_remainder = 9.0 % 2
+
+multiplication_result = division_result * 2
+
+addition_result = division_remainder + multiplication_result
+
+subtraction_result = 9.0  - multiplication_result
+
+floor_result = 9.0 // 2
+
+power_result = multiplication_result ** 3
+
+print("result = " + str(subtraction_result))
